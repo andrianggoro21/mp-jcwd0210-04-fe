dab810b3b122793fb624c58d530d34e284bca27f
andrianggoro21 <andri.anggoro21@gmail.com>
2023-11-13 21:36:19 +0700

    create sidebar

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+
+const Sidebar = () => {
+    return (
+        <Box>
+            
+        </Box>
+    )
+
+}
+
+export default Sidebar;
---
+++ src/pages/product/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import Sidebar from '../../components/sidebar';
+
+const Product = () => {
+    return (
+        <Box>
+            <Sidebar/>
+        </Box>
+    )
+}
+export default Product;
