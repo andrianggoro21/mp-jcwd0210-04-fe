567d16194d2658a6877cbf9ada1df630a045888d
andrianggoro21 <andri.anggoro21@gmail.com>
2023-11-13 20:07:17 +0700

    update graph

--- src/components/areaChart/index.tsx
+++ src/components/areaChart/index.tsx
@@ -1,4 +1,5 @@
 // import React from 'react';
+import axios from 'axios';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -10,6 +11,8 @@ import {
     Filler,
     Legend,
   } from 'chart.js';
+// import { useEffect, useState } from 'react';
+import { useState, useEffect, SetStateAction, Dispatch } from 'react';
 import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
@@ -23,34 +26,134 @@ ChartJS.register(
     Legend
   );
 
-  export const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top' as const,
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
+//   export const options = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top' as const,
+//       },
+//       title: {
+//         display: true,
+//         text: 'Chart.js Line Chart',
+//       },
+//     },
+//   };
+
+//   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+//   export const data = {
+//     labels,
+//     datasets: [
+//       {
+//         fill: true,
+//         label: 'Dataset 2',
+//         data: labels.map(() => Math.floor(Math.random() * 1000)),
+//         borderColor: 'rgb(53, 162, 235)',
+//         backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//       },
+//     ],
+//   };
+
+//   export function AreaChart() {
+//     return <Line options={options} data={data} />;
+//   }
+
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
     },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+interface ChartData {
+  labels: number[];
+  datasets: {
+    fill: boolean;
+    label: string;
+    data: number[];
+    borderColor: string;
+    backgroundColor: string;
+  }[];
+}
+
+interface DataPoint {
+  label: number;
+  price: number;
+}
+
+const initialChartData: ChartData = {
+  labels: [],
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+      data: [],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const AreaChart = () => {
+  const [chartData, setChartData]: [ChartData, Dispatch<SetStateAction<ChartData>>] = useState(
+    initialChartData
+  );
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/report/');
+      const responseData = response?.data?.data;
+      console.log(responseData);
+      
+
+      if (!responseData) {
+        console.error('Invalid response data');
+        return;
+      }
+
+    
+      const dataPoints : DataPoint[] = responseData.map((data: any) => {
+        const timestamp = data.date;
+        const dateObject = new Date(timestamp);
+        const label = dateObject.getMonth() + 1;
+  
+        return {
+          label,
+          price: data.totalPrice,
+        };
+      });
+
+      const labels = dataPoints.map((dataPoint) => dataPoint.label);
+      const prices = dataPoints.map((dataPoint) => dataPoint.price);
+
+      const newData: ChartData = {
+        labels,
+        datasets: [
+          {
+            fill: true,
+            label: 'Dataset 2',
+            data: prices,
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ],
+      };
+      setChartData(newData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  export const data = {
-    labels,
-    datasets: [
-      {
-        fill: true,
-        label: 'Dataset 2',
-        data: labels.map(() => Math.floor(Math.random() * 1000)),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
-  export function AreaChart() {
-    return <Line options={options} data={data} />;
-  }
+  return <Line options={options} data={chartData} />;
+};
