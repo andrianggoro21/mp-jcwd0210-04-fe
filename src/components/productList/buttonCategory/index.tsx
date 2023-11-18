bc98e80de132405e97287bb2a47154ffd1afc3f2
andrianggoro21 <andri.anggoro21@gmail.com>
2023-11-18 23:43:35 +0700

    update

--- src/components/productList/buttonCategory/index.tsx
+++ src/components/productList/buttonCategory/index.tsx
@@ -28,46 +28,46 @@ const ButtonCategory : React.FC<ButtonCategoryProps> = ({onCategoryChange}) => {
     return (
         <Box mb='30px'>
             <Box display='flex' alignItems='center' gap='36px'>
-                <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(null)}>
+                <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(null)} _hover={{bgColor: "#FFF7F3"}} border='none' _focus={{border: "none", boxShadow: "none",}}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src='../../../../public/images/allmenu.svg' />
-                        <Text color='#B4B4B4'>All Menu</Text>
+                        <Text color='#FF7940'>All Menu</Text>
                     </Box> 
                 </Button>
-                <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(1)}>
+                <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(1)} _hover={{bgColor: "#FFF7F3"}} border='none' _focus={{border: "none", boxShadow: "none",}}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[1] as any)?.image}`} />
-                        <Text color='#B4B4B4'>{(category[1] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[1] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
                 <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(2)}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[2] as any)?.image}`}/>
-                        <Text color='#B4B4B4'>{(category[2] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[2] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
                 <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(3)}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[3] as any)?.image}`} />
-                        <Text color='#B4B4B4'>{(category[3] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[3] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
                 <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(4)}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[4] as any)?.image}`} />
-                        <Text color='#B4B4B4'>{(category[4] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[4] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
                 <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(5)}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[5] as any)?.image}`} />
-                        <Text color='#B4B4B4'>{(category[5] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[5] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
                 <Button w='100px' h='100px' bgColor='#FFFFFF' onClick={() => onCategoryChange(6)}>
                     <Box w='100%' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
                         <Image src={`${import.meta.env.VITE_APP_IMAGE_URL}/category/${(category[6] as any)?.image}`} />
-                        <Text color='#B4B4B4'>{(category[6] as any)?.categoryName}</Text>
+                        <Text color='#FF7940'>{(category[6] as any)?.categoryName}</Text>
                     </Box> 
                 </Button>
             </Box>
